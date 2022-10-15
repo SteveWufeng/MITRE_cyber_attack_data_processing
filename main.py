e44eab9d4d41ac0e5d947c28ebb6f63dddf83f12
Steve Wufeng <sw5874@rit.edu>
2022-10-15 14:04:59 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# 
+# @Author: Steve Wufeng
+import pandas as pd
+
+class csv_appender:
+    """processes a csv file and appends each line to another
+        csv file.
+    """
+    __slots__ = ["__source_csv", "__destination_csv"]
+    
+    def __init__(self, source_csv: str, destination_csv: str) -> None:
+        """constructor for a csv_appender object.
+        
+        Args:
+            source_csv (str): source of the csv file to be processed.
+            destination_csv (str): destination csv file to append to.
+        """
+        # initialize filename variables
+        # open the source csv file
+        
+
+
+if __name__ == "__main__":
+    pass
