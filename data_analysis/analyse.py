956aa0c7e59cc6951d20dc56b8b4aa2cc6b3c0d3
SteveWu <86623538+SteveWufeng@users.noreply.github.com>
2022-11-04 00:58:36 -0400

    stubed out some plan for data analysis

---
+++ data_analysis/analyse.py
@@ -0,0 +1,46 @@
+# 
+# file: analyse.py
+# TODO: add description
+# description: This script will analyse the data in the csv file...
+# @Author: Steve Wufeng
+
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+
+
+def get_most_frequent_tactics():
+    # TODO: account all tactics to one column and
+    #       use dataframe.mode to get the most frequent tactics
+
+    return
+
+def get_total_num_of_tactics():
+    """
+    get the total count of each tactic
+    """
+    return
+    
+def get_most_frequent_union_tactics():
+    """
+    Use a dictionary to find which combination of tactics
+    appears most frequently.
+    sort tuple(the key) alphabetically before adding to dictionary
+    """
+    return
+
+def visualize_data():
+    """
+    think about how we can visualize the csv data
+        1. distribution of tactics (bar graph)
+        2. most frequently seen words in descriptions (word cloud)
+        3. most frequent combinations of tactics (bar graph)
+    """
+    return
+
+def main():
+    return
+    
+
+if __name__ == "__main__":
+    main()
